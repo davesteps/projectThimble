892ccded4baa0f9d9cafa6e29d40cf31c1876be5
davesteps <davesteps@gmail.com>
2018-03-03 17:33:30 +0000

    runMultiple

--- runMultiple.py
+++ runMultiple.py
@@ -14,53 +14,39 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # todo - Dates rather than index
-# t
+# traing.py l131
+# for i in range(len(names)):
+    # arrays[i] = np.expand_dims(arrays[i].squeeze(), 0)
 
-sym =  'BRSC'#, 'MSFT', 'GOOGL']
+sym =  ['AAPL', 'MSFT', 'GOOGL']
 
 # User pandas_reader.data.DataReader to load the desired data. As simple as that.
-start = '2000-01-01'
+start = '2005-01-01'
 end = '2018-03-01'
-panel_data = data.DataReader([sym], 'google', start, end)
+symbols = data.DataReader(sym, 'google', start, end).ix['Close']
 
-symbols = panel_data.ix['Close'][sym]
+# symbols.plot()
 
-symbols.plot()
+env = MarketMultiple(symbols)
+env.observation.shape
 
-env = Market(symbols)
+env.observation.squeeze()
 
-env.max_steps
-env.current_index
-env.current_price
-
-mem_len = 1
+# mem_len = 1
 
 nb_actions = env.action_space.n
 
-# Next, we build a very simple model.
-# Convolution1D(nb_filter=nb_filter, filter_length=filter_length, activation='relu',
-#               input_shape=(window_size, nb_input_series)),
-# MaxPooling1D(),  # Downsample the output of convolution by 2X.
-# Convolution1D(nb_filter=nb_filter, filter_length=filter_length, activation='relu'),
-# MaxPooling1D(),
-# Flatten(),
-# Dense(nb_outputs, activation='linear'),
-
+input_shape = env.observation_space.shape
+input_shape
 model = Sequential()
 model.add(Permute((2, 1), input_shape= env.observation_space.shape))
+# model.add(Permute((1,3,2), input_shape= (mem_len,) + env.observation_space.shape))
 model.add(Conv1D(32,10))
 model.add(Activation('relu'))
 model.add(MaxPool1D())
 model.add(Conv1D(32,5))
 model.add(Activation('relu'))
-model.add(MaxPool1D())
-# model.add(Permute((2, 3, 1), input_shape=(mem_len,) + env.observation_space.shape))
-# model.add(Conv2D(32, (8, 8), strides=(4, 4)))
-# model.add(Activation('relu'))
-# model.add(Conv2D(64, (4, 4), strides=(2, 2)))
-# model.add(Activation('relu'))
-# model.add(Conv2D(64, (3, 3), strides=(1, 1)))
-# model.add(Activation('relu'))
+# model.add(MaxPool1D())
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
@@ -68,9 +54,7 @@ model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
 
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
-memory = SequentialMemory(limit=500000, window_length=1)
+memory = SequentialMemory(limit=5000,window_length=1)
 
 # policy = BoltzmannQPolicy(tau=0.05)
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
@@ -80,12 +64,8 @@ agent = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memor
                  nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
                  train_interval=4, delta_clip=1.)
 agent.compile(Adam(lr=.00025), metrics=['mae'])
-
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
 agent.fit(env, nb_steps=1e5, log_interval=10000,verbose=2)
-agent.save_weights('wghts',overwrite=True)
+# agent.save_weights('wghts',overwrite=True)
 # agent.load_weights('model8_6c01ba5f786ebd5938ca8e8f75008a63d2fe11bd/dqn_nav2_30x30x3_2conv_1e7_mvhz')
 
 agent.test(env,nb_episodes=1,visualize=False)
--- tradeEnv.py
+++ tradeEnv.py
@@ -10,7 +10,6 @@ import os
 from random import randint
 
 
-
 class Market(gym.Env):
 
     def __init__(self, symbols, obs_window=130, max_steps=260, test_mode=False):
@@ -113,3 +112,102 @@ class Market(gym.Env):
     #     plt.imshow(self.observation)
     #     plt.savefig('images/' + str(self.step_count) + ".png")
     #
+
+
+class MarketMultiple(gym.Env):
+
+    def __init__(self, symbols, obs_window=130, max_steps=260, test_mode=False):
+
+        self.action_space = spaces.Discrete(symbols.shape[1] + 1)
+        self.observation_space = spaces.Box(low=0., high=1500., shape=(symbols.shape[1], obs_window))  # ,dtype='float64')
+        self.observation = None
+        self.nullaction = symbols.shape[1]
+        self.symbols = symbols
+        self.max_steps = max_steps
+        self.obs_window = obs_window
+        self.test_mode = test_mode
+
+        self._reset()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _reset(self):
+
+        if self.test_mode:
+            self.start_index = self.symbols.shape[0] - 261
+        else:
+            self.start_index = randint(260, self.symbols.shape[0] - 260)
+        self.end_index = self.start_index + self.max_steps - 1
+        self.current_index = self.start_index
+        # position = randint(0, 2)
+        # current_price = float(symbols.iloc[current_index, [position]])
+        # open_price = current_price
+        self.total_reward = 0
+        # closed_positions = 0
+
+        self.new_observation()
+
+        return self.observation
+
+    def new_observation(self):
+        self.observation = self.symbols[self.current_index - self.obs_window:self.current_index].as_matrix().transpose()
+
+    def _step(self, action):
+        """Run one timestep of the environment's dynamics.
+        Accepts an action and returns a tuple (observation, reward, done, info).
+        # Arguments
+            action (object): An action provided by the environment.
+        # Returns
+            observation (object): Agent's observation of the current environment.
+            reward (float) : Amount of reward returned after previous action.
+            done (boolean): Whether the episode has ended, in which case further step() calls will return undefined results.
+            info (dict): Contains auxiliary diagnostic information (helpful for debugging, and sometimes learning).
+        """
+
+        assert self.action_space.contains(action)
+        self.current_index += 1
+
+        if action == self.nullaction:
+            reward = 0
+        else:
+            self.current_price = float(self.symbols.iloc[self.current_index, [action]])
+            self.tomorrow_price = float(self.symbols.iloc[self.current_index + 1, [action]])
+            reward = self.get_percent_change(self.current_price,self.tomorrow_price)
+            self.total_reward += reward
+
+        done = False
+        if self.current_index == self.end_index or self.total_reward < -100:
+            done = True
+
+        self._max_change()
+
+        self.new_observation()
+
+        return self.observation, reward, done, {'TR':self.total_reward, 'MC':self.max_change}
+
+    def _max_change(self):
+        ii = [i for i in range(self.symbols.shape[1])]
+        sp = self.symbols.iloc[self.start_index, ii].as_matrix()
+        cp = self.symbols.iloc[self.current_index, ii].as_matrix()
+
+        self.max_change = round(max(((cp - sp) / sp) * 100), 2)
+
+
+    def get_percent_change(self, p1, p2):
+        return round(((p2 - p1) / p1) * 100,1)
+
+
+    #
+    # def render(self, mode='human',close=True):
+    #     # pass
+    #     # fld = '/Users/davesteps/Google Drive/pycharmProjects/keras_RL/'
+    #     if 'images' not in os.listdir():
+    #         os.mkdir('images')
+    #     # for i in range(len(frames)):
+    #     # plt.ioff()
+    #     plt.imshow(self.observation)
+    #     plt.savefig('images/' + str(self.step_count) + ".png")
+    #
+
