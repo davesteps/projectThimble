157a362ac31c6797bb8a6450e6b0446b0702e4d6
davesteps <davesteps@gmail.com>
2019-02-17 21:04:04 +0000

    update

--- R/get_data.R
+++ R/get_data.R
@@ -1,14 +1,8 @@
 
-library(quantmod)
-require(dplyr)
-
-start <- as.Date("2000-01-01")
-end <- as.Date("2018-12-31")
-
-get_data <- function(sym){
+get_data <- function(sym,start,end){
   # sym <- 'AAPL'
   # str(sym)
-  getSymbols(sym, src = "yahoo", from = start, to = end,auto.assign = F) %>%
+  quantmod::getSymbols(sym, src = "yahoo", from = start, to = end,auto.assign = F) %>%
     as.data.frame() %>%
     setNames(c('Open','High','Low','Close','Volume','Adjusted')) %>%
     mutate(Date = as.Date(row.names(.))) %>%
@@ -16,9 +10,3 @@ get_data <- function(sym){
   
 }
 
-sym <- c('AAPL',"GOOGL",'MSFT','^DJI','EUR=X','^FTSE','GBPUSD=X')
-
-df <- lapply(sym, get_data) %>%
-  bind_rows()
-
-saveRDS(df,'outputs/symbol-data.Rdata')
